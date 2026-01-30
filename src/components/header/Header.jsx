384dc2cbccd3e5a0047c2924e7fe6c186950bfcc
alexaniatoma <alexaniatoma@hotmail.com>
2026-01-30 10:42:28 -0300

    feat: adicionando dropdown no menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/logo.png'
 import S from './header.module.scss'
+import { useState } from 'react'
 
 export default function Header() {
+
+    const [menuAberto, setMenuAberto] = useState(false);
+
   return (
     <>
         <header className= {S.header}>
@@ -11,17 +15,22 @@ export default function Header() {
                     <img src={Logo} alt="Logo do logo do site sendo representada por uma mão segurando um coração, simbolizando apoio" />
                 </Link>
             </section>
-                <nav>
-                    <ul>
-                        <Link to="/doacao">Doação</Link>
-                        <Link to="/voluntariado">Voluntariado</Link>
-                        <Link to="/mentoria">Mentoria</Link>
-                        <Link to="/eventos">Eventos</Link>                     
-                    </ul>
+                <nav className={S.nav}>                    
+                    <Link className={S.link} to="/doacao">Doação</Link>
+                    <Link className={S.link} to="/voluntariado">Voluntariado</Link>
+                    <Link className={S.link} to="/mentoria">Mentorias</Link>
+                    <Link className={S.link} to="/eventos">Eventos</Link>                   
                 </nav>
-                <img src="https://avatars.githubusercontent.com/u/118212495?v=4" alt="" />                    
+                <img className={S.imgUser}src="https://avatars.githubusercontent.com/u/118212495?v=4" alt="" 
+                onClick={() => setMenuAberto(!menuAberto)}/>                    
         </header>
-        <Link to="/usuario">Vinicius Bispo</Link>
+        <nav className={menuAberto ? S.navUser : S.menuFechado}>
+            <Link to="/usuario">Vinicius Bispo</Link>
+            <Link >Meu Voluntariado</Link>
+            <Link >Configurações da conta</Link>
+            <Link >Sair</Link>
+        </nav>
+        
     </>
   )
 }
