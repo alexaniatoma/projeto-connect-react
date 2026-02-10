861643a17b465d0b1e8a95ec1691df61e25184c2
alexaniatoma <alexaniatoma@hotmail.com>
2026-02-10 12:20:29 -0300

    style: ajustando o estilo no footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import S from './footer.module.scss';
+import S from './Footer.module.scss';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={S.footer}>
       <p>@2025 Vai na Web & Empower.</p>
       <p>Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos. </p>      
     </footer>
